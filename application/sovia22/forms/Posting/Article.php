a893462d3158fe4fb074db9b95075a3bb8119e85
Maxim Khan-Magomedov <dr.von.ozgg@gmail.com>
2012-08-06 02:03:31 +0400

    see #16, работает просмотр и комментирование статей

---
+++ application/sovia22/forms/Posting/Article.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Date: 30.08.11
+ * Time: 1:54
+ */
+ 
+class Form_Posting_Article extends Form_Posting
+{
+    public function setEntry(Posting_Row $entry)
+    {
+        $this->getElement('title')->setValue($entry->getTitle());
+        $this->getElement('body')->setValue($entry->getBody());
+        $this->getElement('avatar_id')->setValue($entry->getAvatarId());
+    }
+}
