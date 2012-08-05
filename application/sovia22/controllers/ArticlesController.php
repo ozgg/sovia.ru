23dd41c8889a7edc0d901d28fa1b21d3dc91023c
Maxim Khan-Magomedov <dr.von.ozgg@gmail.com>
2012-08-06 02:03:31 +0400

    see #16, работает просмотр и комментирование статей

--- application/sovia22/controllers/ArticlesController.php
+++ application/sovia22/controllers/ArticlesController.php
@@ -36,5 +36,136 @@ class ArticlesController extends Ext_Controller_Action
             $href = $this->_url(array(), 'articles', true);
             $this->_headLink(array('rel' => 'canonical', 'href' => $href));
         }
+        $this->view->assign('canAdd', $this->_user->getRank() > 1);
+    }
+
+    public function entryAction()
+    {
+        $id    = intval($this->_getParam('id', 0));
+        $alias = $this->_getParam('alias');
+        if ($id > 0) {
+            $view = $this->view;
+            $table  = new Posting();
+            $mapper = $table->getMapper();
+            $mapper->article()->id($id);
+            /** @var $entry Posting_Row */
+            $entry = $mapper->fetchRowIfExists('Такой записи нет');
+            if (!$entry->canBeSeenBy($this->_user)) {
+                $this->_forward('denied', 'error');
+            }
+            $realAlias = $entry->getAlias();
+            if ($this->_getParam('canonical', false) || ($realAlias != $alias)) {
+                $parameters = array('id' => $id, 'alias' => $entry->getAlias());
+                $href = $this->_url($parameters, 'articles_entry', true);
+                $this->_headLink(array('rel' => 'canonical', 'href' => $href));
+            }
+            $view->assign('entry', $entry);
+            $this->_headTitle($entry->getTitle());
+            $this->setDescription($entry->getDescription());
+        } else {
+            $this->_redirect($this->_url(array(), 'dreams', true));
+        }
+        $this->view->assign('canComment', $this->_user->getIsActive());
+        $this->view->assign('avatars',    $this->_user->getAvatars());
+    }
+
+    public function newAction()
+    {
+        if (!$this->_user->getIsActive() || $this->_user->getRank() < 1) {
+            $this->_forward('denied', 'error');
+        }
+        $this->_headTitle('Добавить статью');
+        $form = new Form_Posting_Article();
+        $form->setUser($this->_user);
+        $form->setAction($this->_url(array(), 'articles_new', true));
+        $this->view->assign('form', $form);
+        /** @var $request Zend_Controller_Request_Http */
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $data = $request->getPost();
+            if ($form->isValid($data)) {
+                $this->_edit($data);
+            }
+        }
+    }
+
+    public function editAction()
+    {
+        if (!$this->_user->getIsActive()) {
+            $this->_forward('denied', 'error');
+        }
+        $this->_headTitle('Редактирование');
+        $id = $this->_getParam('id');
+        $this->view->assign('message', $this->_getFlashMessage());
+
+        $table = new Posting();
+        $mapper = $table->getMapper();
+        /** @var $entry Posting_Row */
+        $entry = $mapper->article()->id($id)->fetchRowIfExists();
+
+        if (!$entry->canBeEditedBy($this->_user)) {
+            $this->_forward('denied', 'error');
+        }
+        $form  = new Form_Posting_Article();
+        $form->setUser($this->_user);
+
+        /** @var $request Zend_Controller_Request_Http */
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $data = $request->getPost();
+            if ($form->isValid($data)) {
+                $this->_edit($data, $entry);
+            }
+        } else {
+            if (!empty($entry)) {
+                $form->setEntry($entry);
+            }
+        }
+        $this->view->assign('form', $form);
+    }
+
+    /**
+     * Редактирование записи и создание новой
+     *
+     * @param array $data данные формы
+     * @param Posting_Row|null $entry
+     * @return void
+     */
+    protected function _edit(array $data, Posting_Row $entry = null)
+    {
+        $owner = (is_null($entry) ? $this->_user : $entry->getOwner());
+        if (isset($data['avatar_id'])) {
+            $table = new User_Avatar();
+            /** @var $avatar User_Avatar_Row */
+            $avatar = $table->selectBy('id', $data['avatar_id'])->fetchRow();
+            if (!is_null($avatar)) {
+                if (!$avatar->belongsTo($owner)) {
+                    $data['avatar_id'] = null;
+                }
+            } else {
+                $data['avatar_id'] = null;
+            }
+        }
+        $data['type']         = Posting_Row::TYPE_ARTICLE;
+        $data['community_id'] = 2;
+        $data['is_internal']  = Posting_Row::VIS_PUBLIC;
+
+        if (is_null($entry)) {
+            /** @var $user User_Row */
+            $user  = $this->_user;
+            $entry = $user->createPosting($data);
+            $this->_setFlashMessage('Статья добавлена');
+        } else {
+            $entry->setData($data);
+            $entry->touch();
+            $entry->save();
+            $this->_setFlashMessage('Статья изменена');
+        }
+
+        $parameters = array(
+            'id'    => $entry->getId(),
+            'alias' => $entry->getAlias()
+        );
+        $this->_redirect($this->_url($parameters, $entry->getRouteName(), true));
     }
 }
