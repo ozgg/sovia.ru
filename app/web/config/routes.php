a2396e57f36b4159f7c2049db7f702274b3b35b8
Maxim Khan-Magomedov <maxim.km@gmail.com>
2013-11-05 13:13:49 +0400

    Movements

--- app/web/config/routes.php
+++ app/web/config/routes.php
@@ -19,4 +19,39 @@ return [
         'controller' => 'about',
         'action'     => 'index',
     ],
+    'tos' => [
+        'uri'        => '/tos',
+        'controller' => 'index',
+        'action'     => 'tos',
+    ],
+    'archive' => [
+        'uri'        => '/dreams/archive',
+        'controller' => 'dreams',
+        'action'     => 'archive',
+    ],
+    'random_dream' => [
+        'uri'        => '/dreams/random',
+        'controller' => 'dreams',
+        'action'     => 'random',
+    ],
+    'articles'     => [
+        'uri'       => '/articles',
+        'controller' => 'articles',
+        'action'     => 'index',
+    ],
+    'dreams'     => [
+        'uri'    => '/dreams',
+        'controller' => 'dreams',
+        'action'     => 'index',
+    ],
+    'dreambook' => [
+        'uri'    => '/dreambook',
+        'controller' => 'dreambook',
+        'action'     => 'index',
+    ],
+    'forum' => [
+        'uri' => '/forum',
+        'controller' => 'forum',
+        'action' => 'index',
+    ],
 ];
