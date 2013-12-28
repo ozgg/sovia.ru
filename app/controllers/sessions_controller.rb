2af8e1aa607b29c2ced3dd29c80c740585979049
Maxim Khan-Magomedov <maxim.km@gmail.com>
2013-12-29 01:33:59 +0400

    Sessions spec and authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,49 @@
+class SessionsController < ApplicationController
+
+  # get /login
+  def new
+    @title = 'Авторизация'
+    unless session[:user_id].nil?
+      redirect_authorized_user
+    end
+  end
+
+  # post /login
+  def create
+    if session[:user_id].nil?
+      authenticate_user
+    else
+      redirect_authorized_user
+    end
+  end
+
+  # delete /logout
+  def destroy
+    if session[:user_id].nil?
+      flash[:message] = t('session.not_logged_in')
+    else
+      session[:user_id] = nil
+      flash[:message] = t('session.logged_out')
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def authenticate_user
+    user = User.find_by_login params[:login]
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:message] = t('session.logged_in_successfully')
+      redirect_to root_path
+    else
+      redirect_to login_path
+      flash[:message] = t('session.invalid_credentials')
+    end
+  end
+
+  def redirect_authorized_user
+    flash[:message] = t('session.already_logged_in')
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Sovia::Application.routes.draw do
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  root 'index#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
