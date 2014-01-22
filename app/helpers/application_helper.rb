fe0996b78eb936ed475a41148daaf47e9d1802e9
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-01-22 23:30:59 +0400

    Displaying user avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,17 @@ module ApplicationHelper
     end
   end
 
+  def user_avatar(user)
+    if user.nil? || user.email.nil? || user.email.empty?
+      image_tag 'smile.png'
+    else
+      hash = Digest::MD5.hexdigest(user.email.downcase)
+      url  = "http://www.gravatar.com/avatar/#{hash}?s=100&amp;d=identicon"
+      image_tag url
+    end
+  end
+
+
   def link_to_dream(dream)
     link_to dream.parsed_title, dream_path(dream)
   end
