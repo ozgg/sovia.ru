e54282f7f500e9cc1d56f20d0c512238d438c796
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-01-23 17:18:05 +0400

    Selecting public dream

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -5,9 +5,18 @@ class Dream < Post
     where(entry_type: TYPE_DREAM).order('id desc')
   end
 
+  def self.random_dream
+    max_id = public_dreams.maximum(:id)
+    public_dreams.where("id >= #{max_id}").first
+  end
+
   private
 
   def set_specific_fields
     self.entry_type ||= TYPE_DREAM
+  end
+
+  def self.public_dreams
+    where(entry_type: TYPE_DREAM, privacy: PRIVACY_NONE)
   end
 end
--- spec/models/dream_spec.rb
+++ spec/models/dream_spec.rb
@@ -8,4 +8,20 @@ describe Dream do
       expect(dream.entry_type).to eq(Dream::TYPE_DREAM)
     end
   end
+
+  context "#random_dream" do
+    it "selects random public dream" do
+      3.times { create(:article) }
+      create(:dream)
+      3.times { create(:post) }
+      create(:dream)
+      3.times { create(:protected_dream) }
+      create(:dream)
+      3.times { create(:private_dream) }
+
+      entry = Dream.random_dream
+      expect(entry).to be_a(Dream)
+      expect(entry.privacy).to eq(Dream::PRIVACY_NONE)
+    end
+  end
 end
