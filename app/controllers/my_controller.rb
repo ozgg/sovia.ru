92580764808f06c5b4f2d4dcced430ebf9a29a3f
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-01-26 01:59:41 +0400

    My controller

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -1,7 +1,22 @@
 class MyController < ApplicationController
+  before_action :allow_authorized_only
+
+  # get /my
   def index
   end
 
+  # get /my/dreams
   def dreams
+    page = params[:page] || 1
+    @dreams = Dream.recent.where(user: @current_user).page(page).per(5)
+  end
+
+  private
+
+  def allow_authorized_only
+    unless @current_user
+      flash[:message] = t('please_log_in')
+      redirect_to login_path
+    end
   end
 end
--- spec/controllers/my_controller_spec.rb
+++ spec/controllers/my_controller_spec.rb
@@ -5,8 +5,13 @@ describe MyController do
     before(:each) { session[:user_id] = nil }
 
     shared_examples "login redirector" do
-      it "redirects to login path"
-      it "adds flash message #{I18n.t('please_log_in')}"
+      it "redirects to login path" do
+        expect(response).to redirect_to(login_path)
+      end
+
+      it "adds flash message #{I18n.t('please_log_in')}" do
+        expect(flash[:message]).to eq(I18n.t('please_log_in'))
+      end
     end
 
     context "get index" do
@@ -29,19 +34,52 @@ describe MyController do
     context "get index" do
       before(:each) { get :index }
 
-      it "renders my/index"
+      it "renders my/index" do
+        expect(response).to render_template('my/index')
+      end
     end
 
     context "get dreams" do
-      before(:each) { get :dreams }
+      it "renders my/dreams" do
+        get :dreams
+        expect(response).to render_template('my/dreams')
+      end
 
-      it "renders my/dreams"
-      it "adds public user dreams to @dreams"
-      it "adds protected user dreams to @dreams"
-      it "adds private user dreams to @dreams"
-      it "doesn't add anonymous dreams to @dreams"
-      it "doesn't add others protected dreams to @dreams"
-      it "doesn't add others private dreams to @dreams"
+      it "adds public user dreams to @dreams" do
+        dream = create(:dream, user: user)
+        get :dreams
+        expect(assigns[:dreams]).to include(dream)
+      end
+
+      it "adds protected user dreams to @dreams" do
+        dream = create(:protected_dream, user: user)
+        get :dreams
+        expect(assigns[:dreams]).to include(dream)
+      end
+
+      it "adds private user dreams to @dreams" do
+        dream = create(:private_dream, user: user)
+        get :dreams
+        expect(assigns[:dreams]).to include(dream)
+      end
+
+      it "doesn't add anonymous dreams to @dreams" do
+        dream = create(:dream)
+        get :dreams
+        expect(assigns[:dreams]).not_to include(dream)
+      end
+
+      it "doesn't add others protected dreams to @dreams" do
+        dream = create(:protected_dream)
+        get :dreams
+        expect(assigns[:dreams]).not_to include(dream)
+      end
+
+      it "doesn't add others private dreams to @dreams" do
+        dream = create(:private_dream)
+        get :dreams
+        expect(assigns[:dreams]).not_to include(dream)
+      end
     end
   end
 end
