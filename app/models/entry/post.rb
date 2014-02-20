e4bcecd7be623f2d54d3d18c6f9474addc638197
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-02-20 21:45:06 +0400

    Method mathing_tag for entry children

--- app/models/entry/post.rb
+++ app/models/entry/post.rb
@@ -1,2 +1,6 @@
 class Entry::Post < Entry
+
+  def matching_tag(name)
+    Tag::Post.match_or_create_by_name(name)
+  end
 end
--- app/models/entry/thought.rb
+++ app/models/entry/thought.rb
@@ -1,2 +1,6 @@
 class Entry::Thought < Entry
+
+  def matching_tag(name)
+    Tag::Thought.match_or_create_by_name(name)
+  end
 end
