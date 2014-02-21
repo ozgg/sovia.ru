1cd4d1f6aa67d1affa9438f776c223733f8da2d8
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-02-22 02:35:07 +0400

    my/index/index spec

--- spec/views/my/index/index.html.erb_spec.rb
+++ spec/views/my/index/index.html.erb_spec.rb
@@ -1,5 +1,39 @@
 require 'spec_helper'
 
-describe "index/index.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "my/index/index.html.erb" do
+  shared_examples "mandatory links" do
+    it "has link to profile page" do
+      expect(rendered).to have_selector('a', href: my_profile_path)
+    end
+
+    it "has link to user's dreams page" do
+      expect(rendered).to have_selector('a', href: my_dreams_path)
+    end
+  end
+
+  context "when user's email is confirmed" do
+    before(:each) do
+      assign(:current_user, create(:confirmed_user))
+      render
+    end
+
+    it_should_behave_like "mandatory links"
+
+    it "has no link to email confirmation page" do
+      expect(rendered).not_to have_selector('a', href: my_confirmation_path)
+    end
+  end
+
+  context "when user's email is not confirmed" do
+    before(:each) do
+      assign(:current_user, create(:user))
+      render
+    end
+
+    it_should_behave_like "mandatory links"
+
+    it "has link to email confirmation page" do
+      expect(rendered).to have_selector('a', href: my_confirmation_path)
+    end
+  end
 end
