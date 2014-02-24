210b2049cf3d5376c28bed13ea44482bbf6742e6
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-02-24 18:15:02 +0400

    My deams controller spec

---
+++ app/controllers/my/dreams_controller.rb
@@ -0,0 +1,8 @@
+class My::DreamsController < ApplicationController
+  before_action :allow_authorized_only
+
+  def index
+    page = params[:page] || 1
+    @entries = Entry::Dream.where(user: current_user).order('id desc').page(page).per(5)
+  end
+end
