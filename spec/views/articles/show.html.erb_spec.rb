fef9971ae8d0fe9f08ecdd218cc0a216c64fefb0
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-03-13 22:15:56 +0400

    current user in article view

--- spec/views/articles/show.html.erb_spec.rb
+++ spec/views/articles/show.html.erb_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "articles/show.html.erb" do
   it "renders article" do
+    allow(view).to receive(:current_user)
     article = create(:article)
     assign(:entry, article)
     render
