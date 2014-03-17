452c43c4700bea3b22f77adb5615ab0a0c8ecd31
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-03-17 18:10:12 +0400

    Fix #55, links in views

--- spec/views/dreams/random.html.erb_spec.rb
+++ spec/views/dreams/random.html.erb_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "dreams/random.html.erb" do
   it "renders dream" do
+    allow(view).to receive(:current_user)
     assign(:entry, create(:dream))
     render
     expect(rendered).to render_template('entries/_entry')
