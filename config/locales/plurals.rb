cf480638b8fe9256cf30442eb3dd29fc654e39fd
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-03-21 22:34:01 +0400

    Fix #67, main text and better localization

--- config/locales/plurals.rb
+++ config/locales/plurals.rb
@@ -1,22 +1,11 @@
-{ :ru =>
-      { :i18n =>
-            { :plural =>
-                  { :keys => [:one, :few, :other],
-                    :rule => lambda { |n|
-                      if n == 1
-                        :one
-                      else
-                        if [2, 3, 4].include?(n % 10) &&
-                            ![12, 13, 14].include?(n % 100) &&
-                            ![22, 23, 24].include?(n % 100)
-
-                          :few
-                        else
-                          :other
-                        end
-                      end
-                    }
-                  }
+{
+    :ru => {
+        :'i18n' => {
+            :plural => {
+                :rule => lambda { |n|
+                  n % 10 == 1 && n % 100 != 11 ? :one : [2, 3, 4].include?(n % 10) && ![12, 13, 14].include?(n % 100) ? :few : n % 10 == 0 || [5, 6, 7, 8, 9].include?(n % 10) || [11, 12, 13, 14].include?(n % 100) ? :many : :other
+                }
             }
-      }
+        }
+    }
 }
