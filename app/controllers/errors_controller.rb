ab8d02407d6dcf81df17d061b7181fa17b1f99e4
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-03-24 03:17:19 +0400

    Another view fix

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -6,7 +6,7 @@ class ErrorsController < ApplicationController
   end
 
   def show
-    render view: status_code.to_s, :status => status_code
+    render status_code.to_s, :status => status_code
   end
 
   protected
