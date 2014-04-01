b93169d0837884e1dc6808a4e04744151e89cf1f
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-04-01 22:43:45 +0400

    Fixed tagged dreams view spec

--- spec/views/dreams/tagged.html.erb_spec.rb
+++ spec/views/dreams/tagged.html.erb_spec.rb
@@ -4,6 +4,7 @@ describe 'dreams/tagged.html.erb' do
   it "renders entries/list" do
     allow(view).to receive(:current_user)
     assign(:entries, [])
+    assign(:tag, create(:dream_tag))
     render
     expect(rendered).to render_template('entries/_list')
   end
