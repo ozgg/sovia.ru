f02398f2d4f2d201d2f2b01b2d65b0ef0f8ce080
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-07-09 01:14:35 +0400

    see #90, in my statistics

--- app/controllers/my/statistics_controller.rb
+++ app/controllers/my/statistics_controller.rb
@@ -3,7 +3,6 @@ class My::StatisticsController < ApplicationController
 
   # get /my/statistics
   def index
-    @title = t('controllers.my.statistics.index')
   end
 
   # get /my/statistics/symbols
@@ -12,6 +11,5 @@ class My::StatisticsController < ApplicationController
 
     page   = params[:page] || 1
     @tags  = UserTag.joins(:tag).where(where_clause).order('entries_count desc, tags.name asc').page(page).per(20)
-    @title = t('controllers.my.statistics.tags', page: page)
   end
 end
