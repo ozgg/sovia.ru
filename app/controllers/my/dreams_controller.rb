6998191772fe1f7d54c67e3e4da3fc270dead79e
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-07-09 00:57:39 +0400

    see #90, in my dreams

--- app/controllers/my/dreams_controller.rb
+++ app/controllers/my/dreams_controller.rb
@@ -4,6 +4,5 @@ class My::DreamsController < ApplicationController
   def index
     page     = params[:page] || 1
     @entries = Entry::Dream.where(user: current_user).order('id desc').page(page).per(5)
-    @title   = t('controllers.my.dreams.index', page: page)
   end
 end
