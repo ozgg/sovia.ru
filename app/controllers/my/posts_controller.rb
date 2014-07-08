8b7506d6c82354ec4e5a66d362a371e0017b1528
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-07-09 01:02:31 +0400

    see #90, in my posts

--- app/controllers/my/posts_controller.rb
+++ app/controllers/my/posts_controller.rb
@@ -4,6 +4,5 @@ class My::PostsController < ApplicationController
   def index
     page     = params[:page] || 1
     @entries = Entry::Post.where(user: current_user).order('id desc').page(page).per(5)
-    @title   = t('controllers.my.posts.index', page: page)
   end
 end
