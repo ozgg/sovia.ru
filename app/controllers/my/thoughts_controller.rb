ad4e8917654efed07ab904bcd3c718cc18f45c2d
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-07-09 01:16:31 +0400

    see #90, in my thoughts

--- app/controllers/my/thoughts_controller.rb
+++ app/controllers/my/thoughts_controller.rb
@@ -4,6 +4,5 @@ class My::ThoughtsController < ApplicationController
   def index
     page     = params[:page] || 1
     @entries = Entry::Thought.where(user: current_user).order('id desc').page(page).per(5)
-    @title   = t('controllers.my.thoughts.index', page: page)
   end
 end
