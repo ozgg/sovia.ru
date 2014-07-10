5a696ebf8b9da37b789ce932e092f69f6062a560
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-07-10 23:57:50 +0400

    see #90, in articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,20 +4,16 @@ class ArticlesController < ApplicationController
 
   # get /articles
   def index
-    page     = params[:page] || 1
-    @entries = Entry::Article.recent.page(page).per(5)
-    @title   = t('controllers.articles.index', page: page)
+    @entries = Entry::Article.recent.page(params[:page] || 1).per(5)
   end
 
   # get /articles/new
   def new
     @entry = Entry::Article.new
-    @title = t('controllers.articles.new')
   end
 
   # post /articles
   def create
-    @title = t('controllers.articles.new')
     @entry = Entry::Article.new(article_parameters.merge(user: current_user))
     if @entry.save
       flash[:notice] = t('entry.article.created')
@@ -29,17 +25,14 @@ class ArticlesController < ApplicationController
 
   # get /articles/:id
   def show
-    @title = t('controllers.articles.show', title: @entry.title)
   end
 
   # get /articles/:id/edit
   def edit
-    @title = t('controllers.articles.edit')
   end
 
   # patch /articles/:id
   def update
-    @title = t('controllers.articles.edit')
     if @entry.update(article_parameters)
       flash[:notice] = t('entry.article.updated')
       redirect_to verbose_entry_articles_path(id: @entry.id, uri_title: @entry.url_title)
@@ -58,9 +51,7 @@ class ArticlesController < ApplicationController
 
   # get /articles/tagged/:tag
   def tagged
-    page     = params[:page] || 1
-    @entries = tagged_articles.page(page).per(5)
-    @title   = t('controllers.articles.tagged', tag: @tag.name, page: page)
+    @entries = tagged_articles.page(params[:page] || 1).per(5)
   end
 
   private
