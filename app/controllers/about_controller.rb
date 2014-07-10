0e58c1e9c3b09771c386734527042fe9b946b574
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-07-10 23:49:07 +0400

    see #90, in about

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,21 +1,16 @@
 class AboutController < ApplicationController
   def index
-    @title = t('controllers.about.index')
   end
 
   def features
-    @title = t('controllers.about.features')
   end
 
   def changelog
-    @title = t('controllers.about.changelog')
   end
 
   def terms_of_service
-    @title = t('controllers.about.terms_of_service')
   end
 
   def privacy
-    @title = t('controllers.about.privacy')
   end
 end
