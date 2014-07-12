467397247dd9f931bb04cae628acdf7820ab503a
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-07-12 18:03:07 +0400

    see #90, in statistics

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,13 +1,11 @@
 class StatisticsController < ApplicationController
   # get /statistics
   def index
-    @title = t('controllers.statistics.index')
   end
 
   # get /statistics/symbols
   def symbols
     page   = params[:page] || 1
     @tags  = Tag::Dream.order('entries_count desc, canonical_name asc').page(page).per(20)
-    @title = t('controllers.statistics.symbols', page: page)
   end
 end
