490471f393338a2ccc9d6b980571cb45b4c3c58b
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-08-06 00:32:04 +0400

    Grain is always private

--- app/models/entry/grain.rb
+++ app/models/entry/grain.rb
@@ -1,5 +1,13 @@
 class Entry::Grain < Entry
+  after_initialize :set_specific_fields
+
   def matching_tag(name)
     Tag::Grain.match_or_create_by_name(name)
+  end
+
+  private
+
+  def set_specific_fields
+    self.privacy = PRIVACY_OWNER
   end
 end
