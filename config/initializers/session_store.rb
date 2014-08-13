ab40fd97df3c024ea9f128bd9e5a83595c85dc32
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-08-13 23:42:35 +0400

    Session lifetime is 2 weeks now

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Sovia::Application.config.session_store :cookie_store, key: '_sovia_session'
+Sovia::Application.config.session_store :cookie_store, key: '_sovia_session', expire_after: 2.weeks
