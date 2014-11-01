2e0cd13989f7d038fc3b9eec0028fd3fe594294a
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-11-01 19:13:31 +0300

    Fixed #92, added links in journal

--- app/models/deed.rb
+++ app/models/deed.rb
@@ -2,4 +2,12 @@ class Deed < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :user, :name
+
+  def previous_entry
+    Deed.where(user_id: user_id).where("id < #{id}").order('id desc').first
+  end
+
+  def next_entry
+    Deed.where(user_id: user_id).where("id > #{id}").order('id asc').first
+  end
 end
