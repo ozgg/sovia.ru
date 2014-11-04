0565b00b33c127194801c1a5975494a194cacd5c
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-11-04 23:06:31 +0300

    Added admin url to robots.txt, added sorting for tags queue

--- app/controllers/admin/queues_controller.rb
+++ app/controllers/admin/queues_controller.rb
@@ -2,6 +2,6 @@ class Admin::QueuesController < ApplicationController
   before_action { |c| c.demand_role(User::ROLE_EDITOR) }
 
   def tags
-    @tags = Tag::Dream.where(description: nil).order('entries_count desc').page(params[:page] || 1).per(20)
+    @tags = Tag::Dream.where(description: nil).order('entries_count desc, canonical_name asc').page(params[:page] || 1).per(20)
   end
 end
