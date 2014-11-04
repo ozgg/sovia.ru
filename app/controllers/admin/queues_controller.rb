fbb71c78d651ec370857cd4b86030fc56c70237a
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-11-05 00:25:39 +0300

    Progress bar for tags queue

--- app/controllers/admin/queues_controller.rb
+++ app/controllers/admin/queues_controller.rb
@@ -2,6 +2,14 @@ class Admin::QueuesController < ApplicationController
   before_action { |c| c.demand_role(User::ROLE_EDITOR) }
 
   def tags
-    @tags = Tag::Dream.where(description: nil).order('entries_count desc, canonical_name asc').page(params[:page] || 1).per(20)
+    @tags = tags_in_queue.order('entries_count desc, canonical_name asc').page(params[:page] || 1).per(20)
+    total = Tag::Dream.count
+    @progress = [total - tags_in_queue.count, total]
+  end
+
+  private
+
+  def tags_in_queue
+    Tag::Dream.where(description: nil)
   end
 end
