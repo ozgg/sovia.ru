a57574c1fa5f954791d69ff8313b8ccb295d4c1c
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-11-15 00:53:00 +0300

    Better view of dreambook queue

--- app/controllers/admin/queues_controller.rb
+++ app/controllers/admin/queues_controller.rb
@@ -2,7 +2,7 @@ class Admin::QueuesController < ApplicationController
   before_action { |c| c.demand_role(User::ROLE_EDITOR) }
 
   def tags
-    @tags = tags_in_queue.order('entries_count desc, canonical_name asc').page(params[:page] || 1).per(20)
+    @tags = tags_in_queue.order('entries_count desc, canonical_name asc').page(params[:page] || 1).per(25)
     total = Tag::Dream.count
     @progress = [total - tags_in_queue.count, total]
   end
