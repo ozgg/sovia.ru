02d7196b1a4712d9ccc44310f892b5739b2c1f57
Maxim Khan-Magomedov <maxim.km@gmail.com>
2014-11-16 23:29:40 +0300

    see #114, listing and deleting comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :entry, counter_cache: true, touch: true
   belongs_to :user, counter_cache: true
   belongs_to :parent, class_name: 'Comment'
-  has_many :comments, dependent: :nullify
+  has_many :comments
 
   validates_presence_of :entry, :body
 
