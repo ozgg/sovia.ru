dcc2afd36db7a174feffc9023cda20f18b1d8228
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-03-09 04:40:35 +0300

    see #96, list of post titles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,12 +10,18 @@ class PostsController < ApplicationController
 
   # get /posts/new
   def new
-    @entry = Entry::Post.new
+    @entry = Post.new
   end
 
   # post /posts
   def create
     @entry = Post.new(post_parameters.merge(user: current_user))
+    if @entry.save
+      flash[:notice] = t('entry.post.created')
+      redirect_to @entry
+    else
+      render action: :new
+    end
   end
 
   # get /posts/:id
@@ -46,12 +52,11 @@ class PostsController < ApplicationController
   private
 
   def set_post
-    @entry = Entry::Post.find(params[:id])
-    raise UnauthorizedException unless @entry.visible_to? current_user
+    @entry = Post.find(params[:id].to_i)
   end
 
   def post_parameters
-    params.require(:entry_post).permit(:title, :body, :tags_string)
+    params.require(:post).permit(:language_id, :title, :lead, :body, :image)
   end
 
   def restrict_anonymous_access
@@ -60,32 +65,5 @@ class PostsController < ApplicationController
 
   def restrict_editor_access
     raise UnauthorizedException unless @entry.editable_by? current_user
-  end
-
-  def allowed_posts
-    maximal_privacy = current_user.nil? ? Entry::PRIVACY_NONE : Entry::PRIVACY_USERS
-
-    Entry::Post.recent.where("privacy <= #{maximal_privacy}")
-  end
-
-  def tagged_posts
-    @tag = Tag::Post.match_by_name(params[:tag])
-    raise record_not_found if @tag.nil?
-
-    allowed_posts.joins(:entry_tags).where(entry_tags: { tag: @tag })
-  end
-
-  def emulate_saving
-    flash[:notice] = t('entry.post.created')
-    redirect_to entry_posts_path
-  end
-
-  def create_post
-    if @entry.save
-      flash[:notice] = t('entry.post.created')
-      redirect_to verbose_entry_posts_path(id: @entry.id, uri_title: @entry.url_title)
-    else
-      render action: :new
-    end
   end
 end
