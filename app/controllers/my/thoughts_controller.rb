322ec5168fd8cfe2f00f795eb01f48f7eb06bfff
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-04-07 03:22:24 +0300

    see #137, comments administration

--- app/controllers/my/thoughts_controller.rb
+++
@@ -1,8 +0,0 @@
-class My::ThoughtsController < ApplicationController
-  before_action :allow_authorized_only
-
-  def index
-    page     = params[:page] || 1
-    @entries = Entry::Thought.where(user: current_user).order('id desc').page(page).per(5)
-  end
-end
