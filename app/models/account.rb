5f28e743b302b8f378c4a5fb0bb3c304b2da9e08
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-04-17 17:28:23 +0300

    see #171, managing accounts

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,6 +3,9 @@ class Account < ActiveRecord::Base
 
   belongs_to :user
 
+  has_many :questions, as: :owner, dependent: :destroy
+  has_many :comments, as: :owner
+
   enum network: [:vk]
   enum gender: [:female, :male]
 
@@ -10,6 +13,14 @@ class Account < ActiveRecord::Base
   validates_uniqueness_of :local_id, scope: [:network]
 
   mount_uploader :avatar, AvatarUploader
+
+  def self.networks_for_form
+    networks.keys.to_a.map { |e| [e, e] }
+  end
+
+  def self.genders_for_form
+    [[I18n.t(:not_selected), '']] + genders.keys.to_a.map { |e| [I18n.t("activerecord.attributes.account.genders.#{e}"), e] }
+  end
 
   def set_from_vk_hash(parameters)
     self.name = "#{parameters.first_name} #{parameters.last_name}"
