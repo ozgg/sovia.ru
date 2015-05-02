54f3666c2c1e8933e4ca239eced3e77e5d0ee541
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-05-03 01:52:18 +0300

    see #197, adding and viewing sleep places

--- app/helpers/about_helper.rb
+++
@@ -1,5 +0,0 @@
-module AboutHelper
-  def link_to_ticket(ticket_number)
-    link_to "##{ticket_number}", "https://github.com/ozgg/Sovia/issues/#{ticket_number}", rel: 'external nofollow'
-  end
-end
