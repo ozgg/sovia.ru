f941deecb69e2449ed752492057a6b0085cb88c4
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-05-18 15:35:18 +0300

    Removed old models

--- app/models/entry/post.rb
+++
@@ -1,7 +0,0 @@
-class Entry::Post < Entry
-  validates_presence_of :user
-
-  def matching_tag(name)
-    Tag::Post.match_or_create_by_name(name)
-  end
-end
--- app/models/entry/thought.rb
+++
@@ -1,7 +0,0 @@
-class Entry::Thought < Entry
-  validates_presence_of :user
-
-  def matching_tag(name)
-    Tag::Thought.match_or_create_by_name(name)
-  end
-end
