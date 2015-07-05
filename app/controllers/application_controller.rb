664183370c6c8de31a0d9a05543f14e44a96a76b
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-07-05 16:28:34 +0300

    Helpers for Application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,29 @@
 class ApplicationController < ActionController::Base
+  class UnauthorizedException < Exception
+  end
+
+  class ForbiddenException < Exception
+  end
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_page
+
+  # Get current page from request
+  #
+  # @return [Integer]
+  def current_page
+    @current_page ||= (params[:page] || 1).to_s.to_i.abs
+  end
+
+  protected
+
+  # Wrapper for 'Record not found' exception
+  #
+  # @return [ActiveRecord::RecordNotFound]
+  def record_not_found
+    ActiveRecord::RecordNotFound
+  end
 end
