b8999d0edc7a6109aa67e14fc524b393f1e83a96
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-07-15 01:41:05 +0300

    has_trace spec checks methods

--- spec/support/shared_examples/has_trace.rb
+++ spec/support/shared_examples/has_trace.rb
@@ -1,11 +1,25 @@
 require 'rails_helper'
 
 shared_examples_for 'has_trace' do
-  let(:model) { described_class }
+  let(:model) { described_class.to_s.underscore.to_sym }
+  let(:agent) { create :agent }
 
-  describe 'class definition' do
-    it 'includes has_trace' do
-      expect(model.included_modules).to include(HasTrace)
+  describe '#agent_name' do
+    it 'returns nil when agent is nil' do
+      entity = build model, agent: nil
+      expect(entity.agent_name).to be_nil
+    end
+
+    it 'returns agent name when agent is present' do
+      entity = build model, agent: agent
+      expect(entity.agent_name).to eq(agent.name)
+    end
+  end
+
+  describe '#trace_data' do
+    it 'returns hash with agent name and IP address' do
+      entity = build model, agent: agent, ip: '127.0.0.1'
+      expect(entity.trace_data).to eq({ agent: agent.name, ip: '127.0.0.1' })
     end
   end
 end
