d3bcff88f5d8343287eb0029e2af6f9ae3b170f8
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-07-18 00:04:36 +0300

    Spec for retrieving user by token pair

--- app/models/token.rb
+++ app/models/token.rb
@@ -10,7 +10,15 @@ class Token < ActiveRecord::Base
   validates_presence_of :user_id
   validates_uniqueness_of :token
 
-  def self.user_by_token(token)
+  def self.user_by_token(input)
+    unless input.blank?
+      pair = input.split(':')
+      self.user_by_pair pair[0], pair[1] if pair.length == 2
+    end
+  end
 
+  def self.user_by_pair(user_id, token)
+    instance = self.find_by user_id: user_id, token: token, active: true
+    instance.is_a?(self) ? instance.user : nil
   end
 end
