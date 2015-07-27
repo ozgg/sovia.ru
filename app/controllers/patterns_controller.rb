059ff54d14aed5e5cb428319afe5468238aae45a
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-07-28 00:57:53 +0300

    Pattern management without links

--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -1,2 +1,66 @@
 class PatternsController < ApplicationController
+  before_action :restrict_access
+  before_action :set_entity, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @collection = Pattern.in_languages(visitor_languages).order('slug asc').page(current_page).per(25)
+  end
+
+  def new
+    @entity = Pattern.new
+  end
+
+  def create
+    @entity = Pattern.new creation_parameters
+    if @entity.save
+      set_links
+      redirect_to @entity
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @entity.update entity_parameters
+      set_links
+      redirect_to @entity, notice: t('patterns.update.success')
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @entity.destroy
+      flash[:notice] = t('patterns.delete.success')
+    end
+    redirect_to patterns_path
+  end
+
+  protected
+
+  def restrict_access
+    require_role :administrator
+  end
+
+  def set_entity
+    @entity = Pattern.find params[:id]
+  end
+
+  def entity_parameters
+    params.require(:pattern).permit(:name, :image, :description)
+  end
+
+  def creation_parameters
+    entity_parameters.merge(language_for_entity).merge(owner_for_entity).merge(tracking_for_entity)
+  end
+
+  def set_links
+
+  end
 end
--- spec/controllers/patterns_controller_spec.rb
+++ spec/controllers/patterns_controller_spec.rb
@@ -1,5 +1,124 @@
 require 'rails_helper'
 
-RSpec.describe PatternsController, type: :controller do
+RSpec.describe PatternsController, type: :controller, wip: true do
+  let!(:language) { create :russian_language }
+  let(:user) { create :administrator, language: language }
+  let!(:pattern) { create :pattern, language: language }
 
+  before :each do
+    allow(controller).to receive(:require_role)
+    allow(controller).to receive(:current_user).and_return(user)
+    I18n.locale = language.code
+  end
+
+  shared_examples 'entity_assigner' do
+    it 'assigns pattern to @entity' do
+      expect(assigns[:entity]).to eq(pattern)
+    end
+  end
+
+  describe 'get index' do
+    before(:each) do
+      allow(controller).to receive(:visitor_languages).and_return([pattern.language_id])
+      get :index
+    end
+
+    it_behaves_like 'administrative_page'
+
+    it 'assigns list of patterns to @collection' do
+      expect(assigns[:collection]).to include(pattern)
+    end
+  end
+
+  describe 'get new' do
+    before(:each) { get :new }
+
+    it_behaves_like 'administrative_page'
+
+    it 'assigns new instance Pattern to @entity' do
+      expect(assigns[:entity]).to be_a_new(Pattern)
+    end
+
+    it 'renders view "new"' do
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'post create' do
+    let(:action) { -> { post :create, pattern: attributes_for(:pattern) } }
+
+    context 'authorization and redirects' do
+      before(:each) { action.call }
+
+      it_behaves_like 'administrative_page'
+
+      it 'redirects to created pattern' do
+        expect(response).to redirect_to(Pattern.last)
+      end
+    end
+
+    context 'database change' do
+      it 'inserts row into patterns table' do
+        expect(action).to change(Pattern, :count).by(1)
+      end
+    end
+  end
+
+  describe 'get show' do
+    before(:each) { get :show, id: pattern }
+
+    it_behaves_like 'administrative_page'
+    it_behaves_like 'entity_assigner'
+
+    it 'renders view "show"' do
+      expect(response).to render_template(:show)
+    end
+  end
+
+  describe 'get edit' do
+    before(:each) { get :edit, id: pattern }
+
+    it_behaves_like 'administrative_page'
+    it_behaves_like 'entity_assigner'
+
+    it 'renders view "edit"' do
+      expect(response).to render_template(:edit)
+    end
+  end
+
+  describe 'patch update' do
+    before(:each) do
+      patch :update, id: pattern, pattern: { name: 'new name' }
+    end
+
+    it_behaves_like 'administrative_page'
+    it_behaves_like 'entity_assigner'
+
+    it 'updates pattern' do
+      pattern.reload
+      expect(pattern.name).to eq('new name')
+    end
+
+    it 'redirects to pattern page' do
+      expect(response).to redirect_to(pattern)
+    end
+  end
+
+  describe 'delete destroy' do
+    let(:action) { -> { delete :destroy, id: pattern } }
+
+    context 'authorization' do
+      before(:each) { action.call }
+
+      it_behaves_like 'administrative_page'
+
+      it 'redirects to patterns page' do
+        expect(response).to redirect_to(patterns_path)
+      end
+    end
+
+    it 'removes pattern from database' do
+      expect(action).to change(Pattern, :count).by(-1)
+    end
+  end
 end
