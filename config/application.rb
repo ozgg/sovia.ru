e52f9f16bc59bf687ef6e6607d6b207a62a4e2d1
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-09-14 02:33:08 +0300

    Removed English locale

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,6 @@ module Sovia4
 
     config.i18n.enforce_available_locales = true
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-    config.i18n.available_locales = [:ru, :en]
     config.i18n.default_locale = :ru
 
     config.action_dispatch.rescue_responses.merge!(
