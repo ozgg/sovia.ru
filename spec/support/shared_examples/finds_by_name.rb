3dcc21a1040785da90e33b57f44003187391aa95
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-09-14 02:11:45 +0300

    Removed language from shared examples

--- spec/support/shared_examples/finds_by_name.rb
+++ spec/support/shared_examples/finds_by_name.rb
@@ -7,16 +7,15 @@ shared_examples_for 'finds_by_name' do
     let!(:entity) { create model, name: 'Пример' }
 
     it 'finds entity by canonized name as slug' do
-      expect(described_class.match_by_name 'ПРИМЕР', entity.language).to eq(entity)
+      expect(described_class.match_by_name 'ПРИМЕР').to eq(entity)
     end
   end
 
   describe '#match_or_create_by_name' do
-    let(:language) { create :language }
-    let(:entity) { create model, language: language }
+    let(:entity) { create model }
 
     context 'when entity does not exist' do
-      let(:action) { -> { described_class.match_or_create_by_name 'Чудо-метка', language } }
+      let(:action) { -> { described_class.match_or_create_by_name 'Чудо-метка' } }
 
       it 'creates new entity' do
         expect(action).to change(described_class, :count).by(1)
@@ -29,7 +28,7 @@ shared_examples_for 'finds_by_name' do
 
     context 'when entity exists' do
       it 'returns entity' do
-        expect(described_class.match_or_create_by_name entity.name, language).to eq(entity)
+        expect(described_class.match_or_create_by_name entity.name).to eq(entity)
       end
     end
   end
--- spec/support/shared_examples/has_unique_slug.rb
+++ spec/support/shared_examples/has_unique_slug.rb
@@ -4,9 +4,9 @@ shared_examples_for 'has_unique_slug' do
   let(:model) { described_class.to_s.underscore.to_sym }
 
   context 'validation' do
-    it 'fails with non-unique slug for language' do
+    it 'fails with non-unique slug' do
       entity = create model, name: 'Дубль'
-      expect(build model, language: entity.language, name: 'Дубль!').not_to be_valid
+      expect(build model, name: 'Дубль!').not_to be_valid
     end
   end
 end
