1c11f3f3fed44387bbc2c20029ab4c28c25c10d5
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-10-05 17:09:16 +0300

    Removed obsolete parts from helpers

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,27 +2,4 @@ module CommentsHelper
   def url_to_comment(comment)
     'no url yet'
   end
-
-  def parsed_comment_body(comment, current_user)
-    buffer = ''
-    comment.body.split("\n").each { |string| buffer += process_string string.squish, current_user }
-    buffer
-  end
-
-  protected
-
-  def process_string(string, current_user)
-    if string.blank?
-      ''
-    else
-      "<p>#{quote_and_parse string, current_user}</p>"
-    end
-  end
-
-  def quote_and_parse(string, current_user)
-    quoted_string = string.gsub('<', '&lt;').gsub('>', '&gt;')
-    string_with_dreams = parse_dream_links(quoted_string, current_user)
-    string_with_posts = parse_post_links(string_with_dreams)
-    parse_pattern_links(string_with_posts)
-  end
 end
--- app/helpers/dreams_helper.rb
+++ app/helpers/dreams_helper.rb
@@ -1,24 +1,2 @@
 module DreamsHelper
-  def parsed_dream_body(dream, current_user)
-    buffer, @show_names = '', dream.owned_by?(current_user)
-    dream.body.split("\n").each { |string| buffer += process_string string.squish, current_user }
-    buffer
-  end
-
-
-  protected
-
-  def process_string(string, current_user)
-    if string.blank?
-      ''
-    else
-      "<p>#{quote_and_parse string, current_user}</p>"
-    end
-  end
-
-  def quote_and_parse(string, current_user)
-    quoted_string = string.gsub('<', '&lt;').gsub('>', '&gt;')
-    string_with_dreams = parse_dream_links(quoted_string, current_user)
-    parse_names string_with_dreams
-  end
 end
