0fd389d7f1c2a03247591421722fc900caa791f3
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-10-18 21:40:37 +0300

    Part of markup for dream form

--- app/helpers/dreams_helper.rb
+++ app/helpers/dreams_helper.rb
@@ -1,2 +1,6 @@
 module DreamsHelper
+  def dream_privacy_options
+    prefix = 'activerecord.attributes.dream.privacies'
+    Dream.privacies.keys.to_a.map { |privacy| [ I18n.t("#{prefix}.#{privacy}"), privacy ] }
+  end
 end
