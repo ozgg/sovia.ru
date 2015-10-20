ffd3989d851f677ff727b86acb8689757ed9fad5
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-10-20 21:52:46 +0300

    Markup for posts archive

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
   def title_for_archive(year, month)
-    if year
-      if month
-        ' за ' + t('date.nominative_months')[month.to_i] + " #{year.to_i} года"
-      else
-        " за #{year.to_i} год"
-      end
+    if year && month
+      month_name = t('date.nominative_months')[month.to_i]
+      t('archive_month', year: year.to_i, month: month_name)
+    elsif year
+      t('archive_year', year: year.to_i)
+    else
+      ''
     end
   end
 end
