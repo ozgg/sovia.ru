1551c46c0cfefd6ff0a0c3118045f2e8d4907724
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-11-04 21:36:14 +0300

    Managing browsers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,8 @@ module ApplicationHelper
       ''
     end
   end
+
+  def link_to_delete(entity)
+    link_to t(:delete), entity, method: :delete, data: { confirm: t(:are_you_sure) }
+  end
 end
--- app/models/browser.rb
+++ app/models/browser.rb
@@ -3,4 +3,14 @@ class Browser < ActiveRecord::Base
 
   validates_presence_of :name
   validates_uniqueness_of :name
+
+  PER_PAGE = 25
+
+  def self.page_for_administrator(current_page)
+    order('name asc').page(current_page).per(PER_PAGE)
+  end
+
+  def text_for_list
+    name
+  end
 end
