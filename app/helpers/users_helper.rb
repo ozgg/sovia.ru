c519709760193e757d176620a1de0b6f1c512895
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-11-08 00:24:20 +0300

    Network icon for users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -11,7 +11,7 @@ module UsersHelper
   def user_link(user)
     if user.is_a? User
       text = user.screen_name || user.long_uid
-      link_to text, user_profile_path(uid: user.long_uid), class: "#{user.network} user"
+      link_to text, user_profile_path(uid: user.long_uid), class: "profile #{user.network}"
     else
       I18n.t(:anonymous)
     end
