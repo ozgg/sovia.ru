fb00ff1d1652276b18b1de38712432a093e08bff
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-11-25 00:47:48 +0300

    Fixed spec for helper

--- spec/helpers/patterns_helper_spec.rb
+++ spec/helpers/patterns_helper_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe PatternsHelper, type: :helper do
   describe '#link_to_dreambook' do
     it 'returns link to word in dreambook' do
       pattern = create :pattern
-      expected = link_to pattern.name, dreambook_word_path(letter: pattern.letter, word: pattern.name)
+      expected = link_to pattern.name, dreambook_word_path(letter: pattern.letter, word: pattern.name_for_url)
       expect(helper.link_to_dreambook(pattern)).to eq(expected)
     end
   end
