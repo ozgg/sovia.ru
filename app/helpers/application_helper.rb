71da370314092624c6cf1f204e3ba4968e2f4004
Maxim Khan-Magomedov <maxim.km@gmail.com>
2016-07-20 02:51:04 +0300

    Title for archive in helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,18 @@
 module ApplicationHelper
+  # @param [Integer] year
+  # @param [Integer] month
+  def title_for_archive(year, month)
+    if year && month
+      month_name = t('date.nominative_months')[month.to_i]
+      t('archive_month', year: year.to_i, month: month_name)
+    elsif year
+      t('archive_year', year: year.to_i)
+    else
+      ''
+    end
+  end
+
+  # @param [ApplicationRecord] entity
   def link_to_delete(entity)
     link_to t(:delete), entity, method: :delete, data: { confirm: t(:are_you_sure) }
   end
