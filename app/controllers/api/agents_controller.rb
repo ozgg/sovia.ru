918d17bbc4f12258317e3d118df017b050e0ef35
Maxim Khan-Magomedov <maxim.km@gmail.com>
2016-08-09 22:50:37 +0300

    API controllers for browsers and agents

--- app/controllers/api/agents_controller.rb
+++ app/controllers/api/agents_controller.rb
@@ -1,2 +1,37 @@
 class Api::AgentsController < ApplicationController
+  before_action :restrict_access
+  before_action :set_entity, only: [:toggle, :lock, :unlock]
+
+  # post /api/agents/:id/toggle
+  def toggle
+    if @entity.locked?
+      render json: { errors: { locked: @entity.locked } }, status: :forbidden
+    else
+      render json: { data: @entity.toggle_parameter(params[:parameter].to_s) }
+    end
+  end
+
+  # put /api/agents/:id/lock
+  def lock
+    @entity.update! locked: true
+
+    render json: { data: { locked: @entity.locked? } }
+  end
+
+  # delete /api/agents/:id/lock
+  def unlock
+    @entity.update! locked: false
+
+    render json: { data: { locked: @entity.locked? } }
+  end
+
+  private
+
+  def set_entity
+    @entity = Agent.find_by! id: params[:id], deleted: false
+  end
+
+  def restrict_access
+    require_role :administrator
+  end
 end
--- spec/controllers/api/agents_controller_spec.rb
+++ spec/controllers/api/agents_controller_spec.rb
@@ -1,5 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe Api::AgentsController, type: :controller do
+  let(:user) { create :administrator }
 
+  before :each do
+    allow(subject).to receive(:require_role)
+    allow(subject).to receive(:current_user).and_return(user)
+    allow(Agent).to receive(:page_for_administration)
+  end
+
+  describe 'post toggle' do
+    before(:each) { post :toggle, params: { id: entity, parameter: :mobile } }
+
+    context 'when entity is not locked' do
+      let(:entity) { create :agent }
+
+      it_behaves_like 'page_for_administrator'
+      it_behaves_like 'successful_response'
+
+      it 'toggles parameters' do
+        entity.reload
+        expect(entity).to be_mobile
+      end
+    end
+
+    context 'when entity is locked' do
+      let(:entity) { create :agent, locked: true }
+
+      it_behaves_like 'page_for_administrator'
+      it_behaves_like 'http_forbidden'
+
+      it 'does not toggle parameter' do
+        entity.reload
+        expect(entity).not_to be_mobile
+      end
+    end
+  end
+
+  describe 'put lock' do
+    let(:entity) { create :agent }
+
+    before :each do
+      put :lock, params: { id: entity }
+    end
+
+    it_behaves_like 'page_for_administrator'
+    it_behaves_like 'entity_locker'
+  end
+
+  describe 'delete unlock' do
+    let(:entity) { create :agent, locked: true }
+
+    before :each do
+      delete :unlock, params: { id: entity }
+    end
+
+    it_behaves_like 'page_for_administrator'
+    it_behaves_like 'entity_unlocker'
+  end
 end
