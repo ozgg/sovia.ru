ca15020cdbc5fe54c21185e9e62292669abc7ec6
Maxim Khan-Magomedov <maxim.km@gmail.com>
2016-09-29 03:08:30 +0300

    see #215, dreams archive

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -88,7 +88,7 @@ class DreamsController < ApplicationController
 
   def collect_months
     @dates = Hash.new
-    Dream.visible.distinct.pluck("date_trunc('month', created_at)").sort.each do |date|
+    Dream.not_deleted.distinct.pluck("date_trunc('month', created_at)").sort.each do |date|
       @dates[date.year] = [] unless @dates.has_key? date.year
       @dates[date.year] << date.month
     end
