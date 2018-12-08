0e733b1fa10b956697c16b50d08f8cbd6cf87129
Maxim Khan-Magomedov <maxim.km@gmail.com>
2018-12-08 04:19:20 +0300

    Added components into migrations and compacted migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,28 @@
 Rails.application.routes.draw do
+  # Toggle entity flag
+  concern :toggle do
+    post :toggle, on: :member, defaults: { format: :json }
+  end
+
+  # Check entity state
+  concern :check do
+    post :check, on: :collection, defaults: { format: :json }
+  end
+
+  # Change entity priority
+  concern :priority do
+    post :priority, on: :member, defaults: { format: :json }
+  end
+
+  resources :sleep_places, only: %i[update destroy]
+
   scope '(:locale)', constraints: { locale: /ru|en/ } do
     root 'index#index'
+
+    resources :sleep_places, only: %i[new create edit], concerns: %i[check]
+
+    namespace :my do
+      resources :sleep_places, only: %i[index show]
+    end
   end
 end
--- db/migrate/20181115232022_create_dreams.rb
+++ db/migrate/20181115232022_create_dreams.rb
@@ -1,7 +1,42 @@
+# frozen_string_literal: true
+
+# Dream component and tables
 class CreateDreams < ActiveRecord::Migration[5.2]
   def up
-    return if Dream.table_exists?
+    create_component
+    create_sleep_places unless SleepPlace.table_exists?
+    create_dreams unless Dream.table_exists?
+  end
 
+  def down
+    drop_table :dreams if Dream.table_exists?
+    drop_table :sleep_places if SleepPlace.table_exists?
+
+    BiovisionComponent.where(slug: 'dreams').destroy_all
+    PrivilegeGroup.where(slug: 'dream_managers').destroy_all
+    Privilege.where(slug: 'dream_manager').destroy_all
+  end
+
+  private
+
+  def create_component
+    BiovisionComponent.create(slug: 'dreams', settings: { place_limit: 5 })
+
+    group     = PrivilegeGroup.create(slug: 'dream_managers', name: 'Dream Managers')
+    privilege = Privilege.create(slug: 'dream_manager', name: 'Dream Manager')
+    group.add_privilege(privilege)
+  end
+
+  def create_sleep_places
+    create_table :sleep_places, comment: 'Places where users sleep' do |t|
+      t.timestamps
+      t.references :user, null: false, foreign_key: { on_update: :cascade, on_delete: :cascade }
+      t.integer :dreams_count, default: 0, null: false
+      t.string :name
+    end
+  end
+
+  def create_dreams
     create_table :dreams, comment: 'Dreams of users' do |t|
       t.timestamps
       t.references :user, foreign_key: { on_update: :cascade, on_delete: :cascade }
@@ -13,15 +48,12 @@ class CreateDreams < ActiveRecord::Migration[5.2]
       t.boolean :interpreted, default: false, null: false
       t.integer :lucidity, limit: 2, default: 0, null: false
       t.integer :privacy, limit: 2
+      t.integer :comments_count, default: 0, null: false
       t.string :title
       t.text :body
     end
 
     add_index :dreams, %i[visible privacy]
     execute "create index dreams_created_month_idx on dreams using btree (date_trunc('month', created_at));"
-  end
-
-  def down
-    drop_table :dreams if Dream.table_exists?
   end
 end
