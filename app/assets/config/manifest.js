72ac48feb0259cdd74a0d6c56c6eb0f1ef0e551e
Maxim Khan-Magomedov <maxim.km@gmail.com>
2020-07-05 00:59:13 +0300

    Updated to rails 6; hid dreambook

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,6 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+//= link biovision_base_manifest
 //= link biovision_post_manifest
+//= link biovision_comment_manifest
